460a8c5d304d37e579da6da3de6a12fc92f2e0c8
Evgenii Bezdomnikov <evgenii.bezdomnikov@hemmersbach.com>
2019-03-06 19:47:36 +0100

    wip

--- src/main/java/com/todos/service/bootstrap/DataLoader.java
+++ src/main/java/com/todos/service/bootstrap/DataLoader.java
@@ -20,5 +20,6 @@ public class DataLoader implements CommandLineRunner {
         toDoService.save(new ToDo("2", "test1", false, false));
         toDoService.save(new ToDo("3", "test2", false, false));
         toDoService.save(new ToDo("4", "test3", false, false));
+        toDoService.save(new ToDo("5", "New item", false, false));
     }
 }
